f1a228cfe8399b91126809d8647798034d005bc5
“prasanna kumar” <prasanna.kumar@codecraft.co.in>
2023-08-07 16:30:36 +0530

    implementation of subtraction

--- math.js
+++ math.js
@@ -1,2 +1,11 @@
 // Implementing the math module Addition,Subtraction,Multiplication,Division
 // Here we will write add(),sub(),Mul(), and div()
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns the result
+ */
+function (a,b){
+    return a-b;
+}
