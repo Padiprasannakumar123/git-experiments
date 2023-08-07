eb4328f5dc497b1e5cbc14aabe99a3fecdaf3d39
“prasanna kumar” <prasanna.kumar@codecraft.co.in>
2023-08-07 15:52:42 +0530

    Implementation of Division

--- math.js
+++ math.js
@@ -1,2 +1,15 @@
 // Implementing the math module Addition,Subtraction,Multiplication,Division
 // Here we will write add(),sub(),Mul(), and div()
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns 
+ */
+function div(a,b){
+    return a/b;
+}
+function test(){
+    const expect=5;
+    assert(expect==div(10,2),"divide 10 by 2 is 5");
+}
